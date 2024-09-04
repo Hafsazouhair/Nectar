58961bd9eff5ae491b6b6a41fbec4952d91c333c
hafsazouhair <roshafsa9@gmail.com>
2024-09-04 17:40:06 +0100

    commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <header-app />
+    <div></div>
+    <header2-app />
+
+    <main-app />
+    <product-app />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Header2 from './components/Header2.vue'
+import Main from './components/Main.vue'
+import Product from './views/Products.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'header-app': Header,
+    'header2-app': Header2,
+    'main-app': Main,
+    'product-app': Product,
+  }
+}
+</script>
